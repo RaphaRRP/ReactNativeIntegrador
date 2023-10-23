1abf1577d5bebe328127493794583b0ba1c7c478
Rapharrp <raphaelrrprates@gmail.com>
2023-10-23 11:17:01 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const YourComponent = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Função para fazer a solicitação à API Django
+    const fetchDataFromAPI = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/v1/clientes/');
+        setData(response.data); // Armazena os dados da resposta no estado
+        console.error(response.data[0]);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    };
+
+    fetchDataFromAPI(); // Chama a função ao montar o componente
+  }, []); // O segundo argumento vazio garante que isso seja executado apenas uma vez
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text></Text>
+      <Text>Dados da API Django:</Text>
+      {data ? (
+        <Text>{JSON.stringify(data[1]["codigo"], null, 2)}</Text>
+      ) : (
+        <Text>Carregando...</Text>
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default YourComponent;
