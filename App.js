5918b0ade579cf12d11fded4a24541d6b6d5d99b
RAPHAEL RAMIRES RODRIGUES PRATES <49397032828>
2023-10-25 11:29:04 -0300

    seilá

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import Home from "./src/screens/home";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Home from "./src/screens/Home";
 import Icons from "./src/screens/Icons";
-import List from "./src/screens/List";
+import Lists from "./src/screens/Lists"; 
+
+const Stack = createNativeStackNavigator();
 
 export default function App(){
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name='Home' component={Home}/>
-				<Stack.Screen name='Icons' component={Incons}/>
-				<Stack.Screen name='Lists' component={Lists}/>
+			<Stack.Navigator screenOptions={{
+				headerShown: false, 
+			}}>
+				<Stack.Screen 
+					name='Home'
+					component={Home} 
+				/>
+				<Stack.Screen name='Icons' component={Icons}/>
+				<Stack.Screen name='Lists' component={Lists}/> 
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
